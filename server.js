ee34834d21167279a1d389ac789bab4b6143d486
cindytran <cindyztran@gmail.com>
2021-07-08 15:03:17 -0700

    restricts access to routes to users

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ const beerSchema = new mongoose.Schema({
     rating: String,
     city: String,
     state: String,
-    brewery: String
+    brewery: String,
+    createdById: String
 }, { timestamps: true });
 
 const Beer = mongoose.model("Beer", beerSchema);
@@ -45,7 +46,34 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
+// Google Firebase Middleware
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./on-tap-1a19c-firebase-adminsdk-kge5f-1a180e147e.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+app.use(async function(req, res, next) {
+    const token = req.get('Authorization');
+
+    if(!token) return next();
+
+    const user = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
+    if(user) {
+        req.user = user
+    } else {
+        return res.status(401).json({error: 'token invalid'})
+    }
+    next(); //allows cors to call next piece of middleware
+});
  
+function isAuthenticated(req, res, next) {
+    if(req.user) return next();
+    res.status(401).json({error: 'Please login first.'});
+}
 
 //Routes
 
@@ -68,17 +96,17 @@ app.get('/beers', async (req, res) => {
 
 
 //Beer Delete Route
-app.delete('/beers/:id', async (req, res) => {
+app.delete('/beers/:id', isAuthenticated, async (req, res) => {
     try {
         res.json(await Beer.findByIdAndRemove(req.params.id))
-        
+
     } catch (error) {
         res.status(400).json(error);
     }
 });
 
 //Beer Update Route
-app.put('/beers/:id', async (req, res) => {
+app.put('/beers/:id', isAuthenticated, async (req, res) => {
     try {
         res.json(await Beer.findByIdAndUpdate(req.params.id, req.body, { new: true }));
 
@@ -90,8 +118,9 @@ app.put('/beers/:id', async (req, res) => {
 
 
 //Beer Create Route
-app.post('/beers', async (req, res) => {
+app.post('/beers', isAuthenticated, async (req, res) => {
     try {
+        req.body.createdById = req.user.uid;
         res.json(await Beer.create(req.body));
 
     } catch (error) {
